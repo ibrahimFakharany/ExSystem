d3b76e07848893e09099fdb6386d476af42970ce
ibrahimFakharany <Ibraheemelfakharany@gmail.com>
2021-01-11 23:20:28 +0200

    initial commit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 import pandas as pd 
-
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)   
-
+csrf = CSRFProtect(app)
 if __name__ == "__main__":
     from CourseViews import *
     from common import *
     app.run(debug=True)
+    csrf.init_app(app)
